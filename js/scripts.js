b9ba9eede6a5fbafc28e38cdd226d09945fa2b4e
rahmakevo <siundu344@gmail.com>
2017-06-12 15:39:58 +0300

    add project to git

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.displayDefault();
+
+    var item1 = $("input#item1").val():
+    var item2 = $("input#item2").val();
+    var item3 = $("input#item3").val();
+    var item4 = $("input#item4").val();
+
+    $(".item1").text(item1);
+    $(".item2").text(item2);
+    $(".item3").text(item3);
+    $(".item4").text(item4);
+
+    $(".output").show();
+  })
+})
